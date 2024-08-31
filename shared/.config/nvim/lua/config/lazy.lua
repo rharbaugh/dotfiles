761bc9dd04df6b52c654a2d007c5a2e2ed3790eb
Rob Harbaugh <rob@rharbaugh.com>
2024-08-30 21:55:21 -0400

    vim package updates

--- shared/.config/nvim/lua/config/lazy.lua
+++ shared/.config/nvim/lua/config/lazy.lua
@@ -18,6 +18,15 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
